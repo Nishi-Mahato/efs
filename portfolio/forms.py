9adb16341122e2986b9469f6f983f53fc394d1ee
Nishi mahato <nmahato@unomaha.edu>
2020-07-18 17:39:04 -0500

    My first commit

---
+++ portfolio/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Customer, Stock, Investment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ('cust_number', 'name', 'address', 'city', 'state', 'zipcode', 'email', 'cell_phone',)
+
+
+class StockForm(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = ('customer', 'symbol', 'name', 'shares', 'purchase_price', 'purchase_date',)
+
+
+class InvestmentForm(forms.ModelForm):
+    class Meta:
+        model = Investment
+        fields = ('customer', 'category', 'description', 'acquired_value', 'acquired_date', 'recent_value','recent_date',)
+
+
+class RegisterForm(UserCreationForm):
+    password1 = forms.CharField(label='Password',
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
+    email = forms.EmailField(label='Email', max_length=254, help_text='Required. Enter a valid email address.')
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+
+    class Meta:
+        model = User
+        fields = ('username','email', 'first_name','last_name','password1','password2')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
